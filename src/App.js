38faad0d1b4389f4da2e325dc2f9345feb0e15f5
EmiUngurean <emiungurean1@gmail.com>
2022-05-19 15:50:02 +0300

    render a actor object

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+
+
 function App() {
+  const actor = {
+    name:"Leonardo Dicaprio",
+    score:10,
+    hobbies:"Music and dancing naked in the rain",
+    description:"He is a good guy with a thick mustache"
+  };
+
   return (
     <div className="App">
-      <h1>Haidaaa</h1>
+    <ul>
+      <li>
+        {actor.name}
+      </li>
+      <li>
+        {actor.score}
+      </li>
+      <li>
+        {actor.hobbies}
+      </li>
+      <li>
+        {actor.description}
+      </li>
+    </ul>
     </div>
   );
 }
